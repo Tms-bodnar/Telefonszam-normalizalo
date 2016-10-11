6bf6c6f1c05f4567ae1ecf748b2ac53e2ffeec7b
Tms-bodnar <tms.bodnar@gmail.com>
2016-10-11 22:19:44 +0200

    Add files via upload
    ez az előző verziónál több beviteli furcsaságot kezel

--- TelefonszamNormalizalo.java
+++ TelefonszamNormalizalo.java
@@ -17,32 +17,33 @@ public class TelefonszamNormalizalo {
      */
     public static void main(String[] args) {
 
-        StringBuilder t1 = new StringBuilder("063632281");
-        StringBuilder t2 = new StringBuilder("06-32-425810");
-        StringBuilder t3 = new StringBuilder("06(1)4332810");
-        StringBuilder t4 = new StringBuilder("+3636322810");
-        StringBuilder t5 = new StringBuilder("063032268103");
-        StringBuilder t6 = new StringBuilder("+36-1-4322810");
+        String t1 = "06363228101";
+        String t2 = "06-32-425810";
+        String t3 = "06(1)4332810";
+        String t4 = "+3636322810";
+        String t5 = "30/322681";
+        String t6 = "+36-1-4322810";
+        String[] szamok = new String[6];
+        szamok[0] = t1.replaceAll("[^0-9]","");
+        szamok[1] = t2.replaceAll("[^0-9]","");
+        szamok[2] = t3.replaceAll("[^0-9]","");
+        szamok[3] = t4.replaceAll("[^0-9]","");
+        szamok[4] = t5.replaceAll("[^0-9]","");
+        szamok[5] = t6.replaceAll("[^0-9]","");
+       
         StringBuilder[] telSzam = new StringBuilder[6];
-        telSzam[0] = t1;
-        telSzam[1] = t2;
-        telSzam[2] = t3;
-        telSzam[3] = t4;
-        telSzam[4] = t5;
-        telSzam[5] = t6;
-
         for (int i = 0; i < telSzam.length; i++) {
+            telSzam[i] = new StringBuilder(szamok[i]);
+            if ((telSzam[i].charAt(0) == '3'|| telSzam[i].charAt(0) == '2'||telSzam[i].charAt(0) == '7') && telSzam[i].charAt(1) == '0'){
+                telSzam[i].insert(0, "+36");
+            }
             if (telSzam[i].charAt(0) != '+') {
                 telSzam[i].insert(0, "+");
             }
             if (telSzam[i].charAt(1) == '0') {
                 telSzam[i].replace(1, 2, "3");
             }
-            for (int j = telSzam[i].length() - 1; j >= 0; j--) {
-                if (telSzam[i].charAt(j) == '(' || telSzam[i].charAt(j) == ')' || telSzam[i].charAt(j) == '-') {
-                    telSzam[i].deleteCharAt(j);
-                }
-            }
+
             telSzam[i].insert(3, "-");
             if (telSzam[i].charAt(4) == '1') {
                 telSzam[i].insert(5, "-");
@@ -53,6 +54,20 @@ public class TelefonszamNormalizalo {
                 System.out.print("A(z) "+ telSzam[i]);
                 telSzam[i]= new StringBuilder(" helytelen!");
                 System.out.println(telSzam[i]);
+            }
+            if (telSzam[i].length()==13){
+                if (((telSzam[i].charAt(4) == '3'|| telSzam[i].charAt(4) == '2'||telSzam[i].charAt(4) == '7') && telSzam[i].charAt(5) == '0')){
+                    System.out.print("A(z) "+ telSzam[i]);
+                telSzam[i]= new StringBuilder(" helytelen!");
+                System.out.println(telSzam[i]);
+                }
+            }
+            if (telSzam[i].length()==14){
+                if (!((telSzam[i].charAt(4) == '3'|| telSzam[i].charAt(4) == '2'||telSzam[i].charAt(4) == '7') && telSzam[i].charAt(5) == '0')){
+                    System.out.print("A(z) "+ telSzam[i]);
+                telSzam[i]= new StringBuilder(" helytelen!");
+                System.out.println(telSzam[i]);
+                }
             }
             if (telSzam[i].length()>14){
                 System.out.print("A(z) "+ telSzam[i]);
@@ -66,5 +81,4 @@ public class TelefonszamNormalizalo {
         }
     }
 
-    }
-
+}
